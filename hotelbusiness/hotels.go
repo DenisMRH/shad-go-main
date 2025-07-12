868b88b916765cf2e3f32e51924306eadd22a0ca
DenisMRHD <mrstart151@gmail.com>
2025-07-12 17:34:11 +0300

    Solved hotelbusiness

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -2,6 +2,8 @@
 
 package hotelbusiness
 
+import "sort"
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +15,42 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	type Event struct {
+		Date  int
+		Delta int
+	}
+
+	// Сбор всех изменений
+	var events []Event
+	for _, g := range guests {
+		events = append(events, Event{g.CheckInDate, 1})
+		events = append(events, Event{g.CheckOutDate, -1})
+	}
+
+	// Сортируем по дате. На одной дате выезды (-1) идут первыми.
+	sort.Slice(events, func(i, j int) bool {
+		if events[i].Date == events[j].Date {
+			return events[i].Delta < events[j].Delta
+		}
+		return events[i].Date < events[j].Date
+	})
+
+	var result []Load
+	currGuests := 0
+	prevGuests := -1
+
+	for i := 0; i < len(events); {
+		date := events[i].Date
+		for i < len(events) && events[i].Date == date {
+			currGuests += events[i].Delta
+			i++
+		}
+		// Только если меняется кол-во гостей
+		if currGuests != prevGuests {
+			result = append(result, Load{StartDate: date, GuestCount: currGuests})
+			prevGuests = currGuests
+		}
+	}
+
+	return result
 }
