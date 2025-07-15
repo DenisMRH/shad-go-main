142eb38a431eb5f33e5154c68b5ef7a09a8a2908
DenisMRHD <mrstart151@gmail.com>
2025-07-15 17:34:11 +0300

    Solved hogwarts

--- hogwarts/courselist.go
+++ hogwarts/courselist.go
@@ -3,5 +3,42 @@
 package hogwarts
 
 func GetCourseList(prereqs map[string][]string) []string {
-	return []string{}
+	num := make(map[string]bool)
+	result := []string{}
+
+	for key, prereq := range prereqs {
+		num[key] = true
+		for _, j := range prereq {
+			num[j] = true
+
+		}
+	}
+
+	tempmap := make(map[string]bool)
+
+	for len(result) != len(num) {
+		progress := false
+	outer:
+		for i := range num {
+			if tempmap[i] {
+				continue
+			}
+			for _, j := range prereqs[i] {
+				if !tempmap[j] {
+					continue outer
+				}
+			}
+
+			result = append(result, i)
+			tempmap[i] = true
+			progress = true
+
+		}
+		if !progress {
+			panic("Ошибка")
+		}
+
+	}
+
+	return result
 }
