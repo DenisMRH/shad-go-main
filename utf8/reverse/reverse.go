07d3c77ac95ec47c0e99b96070aa33a49b46db86
DenisMRHD <mrstart151@gmail.com>
2025-07-17 17:34:11 +0300

    Solved utf8

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,20 @@
 
 package reverse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 func Reverse(input string) string {
-	return ""
+	var output strings.Builder
+	output.Grow(len(input))
+
+	for len(input) > 0 {
+		r, size := utf8.DecodeLastRuneInString(input)
+		input = input[:len(input)-size]
+		output.WriteRune(r)
+	}
+
+	return output.String()
 }
--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,34 @@
 
 package spacecollapse
 
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
 func CollapseSpaces(input string) string {
-	return ""
+	var b strings.Builder
+	b.Grow(len(input))
+	inSpace := false
+
+	for len(input) > 0 {
+		r, size := utf8.DecodeRuneInString(input)
+		input = input[size:]
+		if r == utf8.RuneError && size == 1 {
+			r = '\uFFFD'
+		}
+
+		if unicode.IsSpace(r) {
+			if !inSpace {
+				b.WriteByte(' ')
+				inSpace = true
+			}
+		} else {
+			b.WriteRune(r)
+			inSpace = false
+		}
+
+	}
+	return b.String()
 }
